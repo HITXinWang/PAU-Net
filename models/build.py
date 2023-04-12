c107b97769ffc25ee0fe5a83d5da2654b5d1365a
WANG Xin <95744330+HITXinWang@users.noreply.github.com>
2023-04-12 11:04:19 +0800

    Update build.py

--- models/build.py
+++ models/build.py
@@ -1,12 +1,11 @@
 # --------------------------------------------------------
 # --------------------------------------------------------
-from .PDP_Net import PDPNet
+from .PAU_Net import PAUNet
 
-#建模的基本参数
 def build_model(config):
     model_type = config.MODEL.TYPE
-    if model_type == 'pdp-net':
-        model = PDPNet(embed_dim=config.MODEL.NET.EMBED_DIM,
+    if model_type == 'pau-net':
+        model = PAUNet(embed_dim=config.MODEL.NET.EMBED_DIM,
                         patch_size=config.DATA.LPATCH_SIZE,
                         sr_scale=config.MODEL.SR_SCALE,
                         num_blocks=config.MODEL.NUM_BLOCKS)
